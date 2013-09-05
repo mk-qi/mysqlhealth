c8d1ee511c93efd6e3059af6ca46635bf7ec1ede
mk2011 <root@mkrss.com>
2013-09-05 17:08:38 +0800

    1. renamed:    check_mysqhealth_daemon.c -> mysqlhealth.c 2. modified:   Makefile  change check_mysqhealth_daemon to  mysqlhealth 3. remove pidfile handle and sighandle 4. format the code

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC=gcc
-check_mysqhealth_daemon:    check_mysqhealth_daemon.c
-	$(CC) -Wall check_mysqhealth_daemon.c   -o check_mysqhealth_daemon   -L/usr/lib/mysql -lmysqlclient -lz -lm
+mysqlhealth:    mysqlhealth.c
+	$(CC) -Wall mysqlhealth.c   -o mysqlhealth   -L/usr/lib/mysql -lmysqlclient -lz -lm
 static:
-	$(CC) -Wall check_mysqhealth_daemon.c  /usr/lib64/mysql/libmysqlclient.a  -o check_mysqhealth_daemon  -L/usr/lib64 -lpthread -lm -lrt -ldl
+	$(CC) -Wall mysqlhealth.c  /usr/lib64/mysql/libmysqlclient.a  -o mysqlhealth  -L/usr/lib64 -lpthread -lm -lrt -ldl
 clean:
-	rm -rf check_mysqhealth_daemon
+	rm -rf mysqlhealth
 apple:
-	$(CC) -Wall check_mysqhealth_daemon.c   -o check_mysqhealth_daemon   -L/usr/lib/mysql -lmysqlclient -lz -lm
+	$(CC) -Wall mysqlhealth.c   -o mysqlhealth   -L/usr/lib/mysql -lmysqlclient -lz -lm
--- mysqlhealth.c
+++ mysqlhealth.c
@@ -10,7 +10,6 @@
 #include <mysql/errmsg.h>
 #include <string.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -34,28 +33,25 @@
 #define DBNAME     "information_schema"
 #define QUERY      "show processlist"
 
-
-
 /* DAEMON */
 #define PgName   "mysqlhealth"
 #define WorkDir   "/tmp"
-#define PidFile   "/tmp/mysqlhealth.pid"
 
 /* LOG Level */
 #define INFO     1
 #define WARN     2
 #define ERROR    3
 #define DEBUG    4
-
 #define LogFile   "/tmp/mysqlhealth.log"
 
 // FUNCTION DECLARATION
 int  mysqlhealth(char *host, char *user, char *pass, char *dbase, char *query);
+static void handle_connect (int client_socket);
+
 /* Return the UNIX time in microseconds */
 long long ustime(void) {
     struct timeval tv;
     long long ust;
-
     gettimeofday(&tv, NULL);
     ust = ((long long)tv.tv_sec)*1000000;
     ust += tv.tv_usec;
@@ -84,27 +80,18 @@ static char* bad_response  =
   " </body>\r\n"
   "</html>\r\n";
 
-
-static void handle_connect (int client_socket);
-
 void log_msg(int level, const char *fmt, ...) {
     va_list args;
     char msg[1024];
-	
 	va_start(args,fmt);
     if (vsnprintf(msg, sizeof(msg), fmt, args) == -1) {
         msg[sizeof(msg) - 1] = '\0';
-    	
     }
     va_end(args);
-    
     FILE *fp;
     char buf[256];
-    
     fp =  fopen(LogFile,"a");
-
     char* log_type;
-    
     switch(level) {
         case ERROR:
           log_type = "ERROR";
@@ -122,40 +109,16 @@ void log_msg(int level, const char *fmt, ...) {
     /*log time  & formart */
     struct timeval tv;
     gettimeofday(&tv,NULL);
-    
     int off = strftime(buf,sizeof(buf),"%d %b %H:%M:%S.",localtime(&tv.tv_sec));
     snprintf(buf+off,sizeof(buf)-off,"%03d",(int)tv.tv_usec/1000);
     fprintf(fp,"[%d] %s %s %s\n",(int)getpid(),buf,log_type,msg);
     fflush(fp);
     fclose(fp);
 }
-
-// signal_handler 
-void signal_handler(int sig)
-{
-    switch(sig)
-    {
-        case SIGHUP:
-            log_msg(1, "Received SIGHUP signal.");
-            break;
-        case SIGINT:
-        case SIGTERM:
-            log_msg(1, "Received SIGTERM signal. but do nothing");
-            break;
-        default:
-            log_msg(1, "Unhandled signal");
-            break;
-    }
-}
 // DAEMON
 void daemonize() {
-	int pidhandle ,i;
-	
-	char str[10];
-	char *msg;
-    msg=(char*)malloc(50*sizeof(char));
+	int i;
 	if(getppid() == 1) return; //
-	
     switch (fork()) {
            case -1:
                exit(EXIT_FAILURE);
@@ -174,90 +137,43 @@ void daemonize() {
 	}
     
 	//umask(027); 
-
-	pidhandle = open(PidFile,O_RDWR|O_CREAT,0640);
-	if (pidhandle < 0) { 
-		sprintf(msg, "Error while creating lock file: %s!", PidFile);
-		log_msg(2, msg);
-        exit(EXIT_FAILURE);
-	} 
-
-	if (lockf(pidhandle,F_TLOCK,0) < 0) { 
-		sprintf(msg, "Error while locking the file: %s", PidFile);
-		log_msg(2, msg);
-        exit(EXIT_FAILURE);
-	}
-
-	sprintf(str,"%d\n",getpid());
-	
-	if(write(pidhandle,str,strlen(str)) < 0) { // write pid
-		sprintf(msg, "Error while writing pid to pid file: %s", PidFile);
-		log_msg(2, msg);
-		exit(-1);
-	}
-    
     /* close all descriptors */
     for (i = getdtablesize(); i >= 0; --i){
         close(i);
     }
-    /* Open STDIN */
     i = open("/dev/null", O_RDWR);
-    /* STDOUT */
     dup(i);
-    /* STDERR */
     dup(i);
-  
-    signal(SIGCHLD,SIG_IGN); 
-    signal(SIGTSTP,SIG_IGN); 
-    signal(SIGTTOU,SIG_IGN);
-    signal(SIGTTIN,SIG_IGN);
-  
-    signal(SIGHUP, signal_handler);
-	signal(SIGTERM, signal_handler);
-	signal(SIGINT, signal_handler);
-	signal(SIGQUIT, signal_handler);
-    /* Set up a signal handler */
-    free(msg);
-    log_msg(1, "process start running");
-
 }
 
 
 void socket_server(int port) {
-    char msg[100];
     int svr_sock, cli_sock;
-    
 	socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
     svr_sock = socket(AF_INET, SOCK_STREAM, 0);
- 
     if(svr_sock < 0) {
-        sprintf(msg, "error creating socket: %i\n", errno);
-    	log_msg(2, msg);
+        log_msg(2, "error creating socket: %i", errno);
         exit(1);
     }
-     
      int yes=1;
      //set server reuse server address
      if (setsockopt(svr_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
          exit(1);
      }
-	 
 	 serv_addr.sin_family = AF_INET;
 	 serv_addr.sin_addr.s_addr = INADDR_ANY;
 	 serv_addr.sin_port = htons(port);
 	 
 	 if (bind (svr_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		 sprintf(msg, "error binding socket: %i\n", errno);
-		 log_msg(2, msg);
+		 //log_msg(2, "error binding socket: %i\n", errno);
 		 exit(1);
 	 }
-	 
 	 if (listen(svr_sock, 2) != 0) {
-		 sprintf(msg, "error binding socket: %i\n", errno);
-		 log_msg(2, msg);
+		 log_msg(2, "error listen socket: %i", errno);
 		 exit(1);
 	 }
+     log_msg(1,"%s listening on port 5000",PgName);
 	 while (1)
 	 {
 		 clilen = sizeof(cli_addr);
@@ -267,14 +183,10 @@ void socket_server(int port) {
 				 /* The call was interrupted by a signal.  Try again.  */
 				 continue;
 			 else
-				 //printf(msg, "accept error: %i\n", errno);  
-				 sprintf(msg, "accept error: %i\n", errno);
-			 log_msg(2, msg);
+                 log_msg(2, "accept error: %i", errno);
 		 }
 		 getpeername (cli_sock, (struct sockaddr *) &cli_addr, &clilen);
-		 printf ("connection accepted from %s\n",inet_ntoa (cli_addr.sin_addr));
-		 sprintf(msg, "connection accepted from %s", inet_ntoa (cli_addr.sin_addr));
-		 log_msg(1, msg);
+		 log_msg(1, "connection accepted from %s", inet_ntoa (cli_addr.sin_addr));
 		 handle_connect(cli_sock);
 	 }/* while end */
 	 close(svr_sock);
@@ -287,8 +199,7 @@ static void handle_connect (int client_socket)
 	memset(request, 0, buffer_len*sizeof(char));
 	read (client_socket, request, buffer_len-1);
 	char* method = (char*) malloc(8);
-	char* URL = (char*) malloc(128);
-	sscanf(request, "%s %s ", method, URL);
+	sscanf(request, "%s", method);
 	char* response = (char*) malloc(buffer_len);
 	memset(response, 0, buffer_len*sizeof(char));
 	int ret = mysqlhealth(DBSERVER, DBUSER, DBPASS, DBNAME,QUERY);
@@ -304,7 +215,6 @@ static void handle_connect (int client_socket)
 	free(response);
 	free(request);
 	free(method);
-	free(URL);
 	close(client_socket);
 }
 /*  */
@@ -318,34 +228,29 @@ int mysqlhealth(char *host, char *user, char *pass, char *dbase, char *query) {
 		log_msg(2, "MySql Initialization Error!");
     } else {
 		if(mysql_real_connect(&mysql, host, user, pass, dbase, 0, NULL, 0) == NULL) {
-			//printf(msg, "MySql Server Error: %s", mysql_error(&mysql));
-			sprintf(msg, "MySql Server Error: %s", mysql_error(&mysql));
-			log_msg(2, msg);
+			log_msg(2, "MySql Server Error: %s", mysql_error(&mysql));
 			return(2);
 		} else {
 			if (debug){
-				printf(msg, "MySql Server Version: %s", mysql_get_server_info(&mysql));
-			    sprintf(msg, "MySql Server Version: %s", mysql_get_server_info(&mysql));
-                log_msg(4, msg);
+                log_msg(2, "MySql Server Version: %s", mysql_get_server_info(&mysql));
             }
             MYSQL_RES *result;
         	MYSQL_ROW row;
-        	int i, num_fields;
+        	int num_fields;
         	mysql_query(&mysql, query);
             result = mysql_store_result(&mysql);
             num_fields = mysql_num_fields(result);
             while ((row = mysql_fetch_row(result)))
-        	{
+        	{/*
         		for(i = 0; i < num_fields; i++){
                     sprintf(msg,"%s ", row[i] ? row[i] : "NULL");
                     if (debug){
                         log_msg(4, msg);
                     }    
-        		}
+        		}*/
         		//printf("\n");
         	}
-            sprintf(msg,"show processlist with : %.3f seconds",(float)(ustime()-start)/1000000);
-            log_msg(1, msg);
+            log_msg(1,"show processlist with : %.3f seconds",(float)(ustime()-start)/1000000);
             mysql_free_result(result);
 		}
 	}
